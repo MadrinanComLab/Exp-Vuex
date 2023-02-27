c0aecfd51bf639c5984e1f7b66528a2bd7852515
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2023-02-27 21:35:09 +0800

    Simple increment and decrement function was created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+
+        <div class="counter">{{ counter }}</div>
+
+        <div class="buttons">
+            <button @click="decreaseCounter">-</button>
+            <button @click="increaseCounter">+</button>
+        </div>
+    </div>
 </template>
 
 <script>
 
 export default {
   name: 'HomeView',
-  components: {
+  methods:{
+      increaseCounter(){
+          this.counter++;
+      },
+      decreaseCounter(){
+          this.counter--;
+      }
+  },
+  data(){
+      return {
+          counter: 0
+      }
   }
 }
 </script>
+
+<style>
+div{
+    margin-bottom: 10px;
+}
+div.counter{
+    font-size: 80px;
+}
+div.buttons button{
+    font-size: 40px;
+    width: 200px;
+    margin: 0px 10px;
+}
+</style>
